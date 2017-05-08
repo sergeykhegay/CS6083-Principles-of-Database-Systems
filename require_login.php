15ea4a9b802284c32d34014686b66433231b5ab7
Sergey Khegay <g.sergeykhegay@gmail.com>
2017-05-08 18:48:20 -0400

    Add require_login.php

---
+++ require_login.php
@@ -0,0 +1,9 @@
+<?php require_once "./utils.php"; ?>
+<?php
+  $uid = $_SESSION["uid"];
+
+  if (!isset($uid) || !user_exists($uid)) {
+    header("Location: ./user_login.php?login_required=true");
+  }
+
+?>
